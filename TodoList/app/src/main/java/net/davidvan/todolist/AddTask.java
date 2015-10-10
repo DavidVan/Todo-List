815f0c48c3c147547ff2a0109dd208d80baf8703
Kenneth Ung <knnthung@gmail.com>
2015-10-10 12:14:05 -0700

    Creation of the Done button and added functionality.

--- TodoList/app/src/main/java/net/davidvan/todolist/AddTask.java
+++ TodoList/app/src/main/java/net/davidvan/todolist/AddTask.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.CheckBox;
 
 import java.util.ArrayList;
 
@@ -14,22 +15,39 @@ import java.util.ArrayList;
  */
 public class AddTask extends Activity {
 
-    Intent myIntent;
+    /* Variables */
+    public EditText memo1;
+    public CheckBox alert;
+    public String memo2;
+    public boolean setAlert = false;
 
+    public void initTasks(){
+        memo1 = (EditText)findViewById(R.id.task_name_edit_text);
+        alert = (CheckBox)findViewById(R.id.task_alert_button);
+    }
+    public void setValues(){
+        memo2 = memo1.getText().toString();
+        setAlert = alert.isChecked();
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_task);
-
+        initTasks();
         Button next = (Button) findViewById(R.id.task_back_button);
+        setValues();
         next.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                myIntent = new Intent(view.getContext(), MainActivity.class);
+                Bundle b = new Bundle();
+                b.putString("tasks", memo2);
+                b.putBoolean("alert", setAlert);
+                Intent in = new Intent(AddTask.this, MainActivity.class);
+                in.putExtras(b);
                 // Get EditText text.
-                EditText taskName = (EditText) findViewById(R.id.task_name_edit_text);
-                String taskNameContents = taskName.getText().toString();
-                myIntent.putExtra("taskName", taskNameContents);
-                startActivityForResult(myIntent, 0);
+//                EditText taskName = (EditText) findViewById(R.id.task_name_edit_text);
+//                String taskNameContents = taskName.getText().toString();
+//                myIntent.putExtra("taskName", taskNameContents);
+                  startActivityForResult(myIntent, 0);
             }
 
         });
