19981b3fead0617d818f6ca5e6ebd70186d6b5e3
David Van <David@DavidVan.net>
2015-10-10 14:38:24 -0700

    Added changes

--- TodoList/app/src/main/java/net/davidvan/todolist/AddTask.java
+++ TodoList/app/src/main/java/net/davidvan/todolist/AddTask.java
@@ -68,4 +68,28 @@ public class AddTask extends Activity {
 //        super.onStart();
 //    }
 
+    public void addAlert(View view)
+    {
+        boolean hasAlert = ((CheckBox) view).isChecked();
+
+        EditText text = (EditText) findViewById(R.id.date_time);
+
+        switch (view.getId())
+        {
+            case R.id.task_alert_button:
+                if (hasAlert)
+                {
+                    text.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    text.setVisibility(View.GONE);
+                }
+                break;
+        }
+
+
+
+    }
+
 }
